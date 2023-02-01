b5f4fe295accf7077c9b1706f7a7a5cb9aa62102
uthx <rdrahuldhiman.19@gmail.com>
2023-02-01 09:06:12 +0530

    Implemented SQS and Lambda

--- lib/sqs_lambda-stack.ts
+++ lib/sqs_lambda-stack.ts
@@ -1,16 +1,60 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { SqsEventSource } from "aws-cdk-lib/aws-lambda-event-sources";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as sns from "aws-cdk-lib/aws-sns";
+import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as cdk from "aws-cdk-lib";
+import * as path from "path";
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SqsLambdaStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const dlqLambda = new NodejsFunction(this, "dlq-lambda", {
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(5),
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: "main",
+      entry: path.join(__dirname, `/../src/dlq-lambda/index.ts`),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SqsLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const deadLetterQueue = new sqs.Queue(this, "dead-letter-queue", {
+      retentionPeriod: cdk.Duration.minutes(30),
+    });
+
+    dlqLambda.addEventSource(new SqsEventSource(deadLetterQueue));
+
+    const queue = new sqs.Queue(this, "sqs-queue", {
+      deadLetterQueue: {
+        queue: deadLetterQueue,
+        maxReceiveCount: 3,
+      },
+    });
+
+    const topic = new sns.Topic(this, "sns-topic");
+
+    topic.addSubscription(new subs.SqsSubscription(queue));
+
+    const myLambda = new NodejsFunction(this, "my-lambda", {
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(5),
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: "main",
+      entry: path.join(__dirname, `/../src/my-lambda/index.ts`),
+    });
+
+    myLambda.addEventSource(
+      new SqsEventSource(queue, {
+        batchSize: 10,
+      })
+    );
+
+    new cdk.CfnOutput(this, "snsTopicArn", {
+      value: topic.topicArn,
+      description: "The arn of the SNS topic",
+    });
   }
 }
---
+++ src/my-lambda/index.ts
@@ -0,0 +1,16 @@
+import {APIGatewayProxyResultV2, SQSEvent} from 'aws-lambda';
+
+export async function main(event: SQSEvent): Promise<APIGatewayProxyResultV2> {
+  const messages = event.Records.map(record => {
+    const body = JSON.parse(record.body) as {Subject: string; Message: string};
+
+    return {subject: body.Subject, message: body.Message};
+  });
+
+  console.log('messages 👉', JSON.stringify(messages, null, 2));
+
+  return {
+    body: JSON.stringify({messages}),
+    statusCode: 200,
+  };
+}
